392cb391b586034955f1d09770758b51e182e0d7
adarsh-08-byte <adarshsamrat315@gmail.com>
2025-02-12 23:15:54 +0530

    EDITMSG

--- chaiaurdjango/chai/urls.py
+++ chaiaurdjango/chai/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name="home"),  # Remove the space here
-    path('about/', views.about, name="about"),
-    path('contact/', views.contact, name="contact"),
+    
+    path('', views.all_chai, name="all_chai"),  
 ]
